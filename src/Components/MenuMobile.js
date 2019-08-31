57cebcfe253a7c5b382908c5c275464f41618289
Karen Almeida <karenkaka2010@hotmail.com>
2019-08-31 18:06:21 -0300

    adicionando menu versao mobile

---
+++ src/Components/MenuMobile.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import barra from '../Imagem/barra.png'
+import '../Style/MenuMobile.css'
+import icone from '../Imagem/iconeMobile.png'
+import facebook from '../Imagem/facebook.png'
+import instagram from '../Imagem/instagram.png'
+import linkedin from '../Imagem/linkedin.png'
+
+export default class Barra extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            open: false,
+        }
+    }
+    mostrarMenu = () => {
+        this.setState(state => {
+            return {
+                open: !state.open
+            };
+        });
+    };
+    esconderMenu = event => {
+        this.setState(state => {
+            return {
+                open: !state.open
+            };
+        });
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <button onClick={this.mostrarMenu} id="dropdown"><img src={barra} /></button>
+                {this.state.open ?
+                    <ul id="menuItens">
+                        <div id="container">
+                            <hr size="1" />
+                            <li>HOME</li>
+                            <li>EMPRESA</li>
+                            <li>MEJ</li>
+                            <li>SERVIÇOS</li>
+                            <li>EQUIPE</li>
+                            <li>BLOG</li>
+                            <li>ORÇAMENTO</li>
+                            <hr size="1" />
+                        </div>
+                        <div id="icones">
+                            <a href="https://www.facebook.com/EJComp.UNESP/?epa=SEARCH_BOX" target="_blank" rel="noopener noreferrer" ><img id="face" alt="icone" src={facebook} /></a>
+                            <a href="https://www.instagram.com/ejcompunesp/" target="_blank" rel="noopener noreferrer" ><img id="insta" alt="icone" src={instagram} /></a>
+                            <a href="https://pt.linkedin.com/company/ejcomp" target="_blank" rel="noopener noreferrer" ><img id="linkd" alt="icone" src={linkedin} /></a>
+                        </div>
+                        <button onClick={this.esconderMenu} id="icone"><img alt="fechar" src={icone} /></button>
+
+                    </ul>
+                    :
+                    null
+                }
+            </React.Fragment>
+        )
+    }
+}
--- src/Components/TelaHome.js
+++ src/Components/TelaHome.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import '../Style/App.css';
 import { ButtonHome, ButtonEmpresa, ButtonMej, ButtonServiços, ButtonEquipe, ButtonBlog } from './Buttons';
-import '../Style/Icones.css';
-import'../Style/ButtonOrçamento.css'
+import ejcomp from '../Imagem/ejcomp.png'
+import facebook from '../Imagem/facebook.png'
+import instagram from '../Imagem/instagram.png'
+import linkedin from '../Imagem/linkedin.png'
+import MenuMobile from '../Components/MenuMobile'
 
 export default class TelaHome extends React.Component {
   render() {
     return (
       <div className="App">
         <div id="containerLogo">
-          <img src="https://www.freelogodesign.org/file/app/client/thumb/1d3862c6-cb0e-43cd-9565-602498128e3c_200x200.png?1565911256246" alt="logo" /></div>
+          <img src={ejcomp} alt="logo" /></div>
         <div id="containerButtons">
           <ButtonHome nome="HOME" />
           <ButtonEmpresa nome="EMPRESA" />
@@ -18,11 +21,15 @@ export default class TelaHome extends React.Component {
           <ButtonEquipe nome="EQUIPE" />
           <ButtonBlog nome="BLOG" />
         </div>
+        <div id="containerApp"><button id="btn3">APLICATIVOS</button></div>
         <div id="containerButtonOrçamento"><button id="btn2">FAÇA UM ORÇAMENTO</button></div>
+        <div id="menu">
+          <MenuMobile/>
+        </div>
         <div id="containerIcones">
-          <a href="https://www.facebook.com/EJComp.UNESP/?epa=SEARCH_BOX" target="_blank" rel="noopener noreferrer" ><img id="fb" alt="icone" src="https://icon-icons.com/icons2/791/PNG/32/FB_icon-icons.com_65484.png" /></a>
-          <a href="https://www.instagram.com/ejcompunesp/" target="_blank" rel="noopener noreferrer" ><img id="ig" alt="icone" src="https://icon-icons.com/icons2/791/PNG/32/instagram_f_icon-icons.com_65485.png" /></a>
-          <a href="https://pt.linkedin.com/company/ejcomp" target="_blank" rel="noopener noreferrer" ><img id="ld" alt="icone" src="https://icon-icons.com/icons2/793/PNG/32/linkedin_icon-icons.com_65588.png" /></a>
+          <a href="https://www.facebook.com/EJComp.UNESP/?epa=SEARCH_BOX" target="_blank" rel="noopener noreferrer" ><img id="fb" alt="icone" src={facebook} /></a>
+          <a href="https://www.instagram.com/ejcompunesp/" target="_blank" rel="noopener noreferrer" ><img id="ig" alt="icone" src={instagram} /></a>
+          <a href="https://pt.linkedin.com/company/ejcomp" target="_blank" rel="noopener noreferrer" ><img id="ld" alt="icone" src={linkedin} /></a>
         </div>
       </div>
     );
