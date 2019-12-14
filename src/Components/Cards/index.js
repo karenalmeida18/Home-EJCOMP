f881496eedb1a28a852165dc2490705faafdb7f8
Tilteee <42361545+Tilteee@users.noreply.github.com>
2019-12-14 14:38:53 -0300

    Upando arquivos reorganizados
    Passando para grid layout e deixando responsivo

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -1,29 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
 import { Storage, Laptop, Smartphone } from '@material-ui/icons';
 import "./styles.css";
-const Card = () => (
-    <div className="services">
-        <p style={{marginBottom: '10px'}}>O QUE FAZEMOS?</p>
-        <div className="line">
-            
-        </div>
-        <div className="cardsproj">
-            <IconButton>
-                <Laptop style={{fontSize: '150px', color: '#12264A'}}/>
-            </IconButton>
-        </div>
-        <div className="cardsproj">
-            <IconButton>
-                <Smartphone style={{fontSize: '150px',  color: '#12264A'}}/>
-            </IconButton>
-        </div>
-        <div className="cardsproj">
-            <IconButton>
-                <Storage style={{fontSize: '150px', color: '#12264A'}}/>
-            </IconButton> 
-        </div>
-    </div>
-);
 
-export default Card;
+export default class Card extends Component{
+    constructor(props){
+        super(props);
+        this.sites = this.sites.bind(this);
+    }
+    sites(){
+        var div=  document.getElementById('divTeste');
+        div.innerHTML = "<p>Sites</p><br><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>";
+
+        
+    }
+    aplicativos(){
+        var div=  document.getElementById('divTeste');
+        div.innerHTML = "<p>Aplicativos</p><br><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>";
+    }
+    sistemas(){
+        var div=  document.getElementById('divTeste');
+        div.innerHTML = "<p>Sistemas</p><br><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>";
+    }
+    render (){ 
+    return (      
+        <div className="services">
+            <p style={{marginBottom: '10px'}}>O QUE FAZEMOS?</p>
+        <div>
+          <Grid container spacing={2}>
+            <Grid item xs>
+            <div className="cardsproj">
+                <IconButton onClick={this.sites}>
+                    <Laptop style={{fontSize: '150px', color: '#12264A'}}/>
+                </IconButton>
+                
+            </div>
+            </Grid>
+            <Grid item xs>
+            <div className="cardsproj">
+                <IconButton onClick={this.aplicativos}>
+                    <Smartphone style={{fontSize: '150px',  color: '#12264A'}}/>
+                </IconButton>
+            </div>
+            </Grid>
+            <Grid item xs>
+            <div className="cardsproj">
+                <IconButton onClick={this.sistemas}>
+                    <Storage style={{fontSize: '150px', color: '#12264A'}}/>
+                </IconButton> 
+            </div>
+            </Grid>
+          </Grid>
+          </div>
+          <div id = "divTeste"></div>
+        </div>
+        
+    );
+}
+}
+    
+
