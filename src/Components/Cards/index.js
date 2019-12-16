dc59cfef0d950a88fefca2092c2af75d9bbeb2c0
Tilteee <42361545+Tilteee@users.noreply.github.com>
2019-12-16 18:40:08 -0300

    Add files via upload

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import Grid from '@material-ui/core/Grid';
 import { Storage, Laptop, Smartphone } from '@material-ui/icons';
-import "./styles.css";
+import Navbar from '../Navbar/Navbar'
+import Logo from '../../Imagem/ejcomp.png'
+import MenuMobile from '../MenuMobile/MenuMobile'
+import "./cards.css";
 
 export default class Card extends Component{
     constructor(props){
@@ -10,52 +13,62 @@ export default class Card extends Component{
         this.sites = this.sites.bind(this);
     }
     sites(){
-        var div=  document.getElementById('divTeste');
-        div.innerHTML = "<p>Sites</p><br><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>";
-
+        var div=  document.getElementById('divSites');
+        div.innerHTML = "<p>Sites</p><br><h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>";
+        
         
     }
     aplicativos(){
-        var div=  document.getElementById('divTeste');
-        div.innerHTML = "<p>Aplicativos</p><br><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>";
+        var div=  document.getElementById('divApps');
+        div.innerHTML = "<p>Aplicativos</p><br><h4>Borem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>";
     }
     sistemas(){
-        var div=  document.getElementById('divTeste');
-        div.innerHTML = "<p>Sistemas</p><br><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>";
+        var div=  document.getElementById('divSystem');
+        div.innerHTML = "<p>Sistemas</p><br><h4>Corem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>";
     }
     render (){ 
-    return (      
+    return (   
+        <>   
+        <Navbar color='white' logo={Logo} className='itemMenu'/>
+        <div id="menuCards"> 
+            <MenuMobile/> 
+        </div>
         <div className="services">
             <p style={{marginBottom: '10px'}}>O QUE FAZEMOS?</p>
         <div>
+            <div className="line">
+                <hr/>
+            </div>
           <Grid container spacing={2}>
             <Grid item xs>
             <div className="cardsproj">
-                <IconButton onClick={this.sites}>
+                <IconButton onMouseEnter={this.sites}>
                     <Laptop style={{fontSize: '150px', color: '#12264A'}}/>
                 </IconButton>
-                
             </div>
+            <div id="divSites"></div>
             </Grid>
             <Grid item xs>
             <div className="cardsproj">
-                <IconButton onClick={this.aplicativos}>
+                <IconButton onMouseEnter={this.aplicativos}>
                     <Smartphone style={{fontSize: '150px',  color: '#12264A'}}/>
                 </IconButton>
             </div>
+            <div id = "divApps"></div>
             </Grid>
             <Grid item xs>
             <div className="cardsproj">
-                <IconButton onClick={this.sistemas}>
+                <IconButton onMouseEnter={this.sistemas}>
                     <Storage style={{fontSize: '150px', color: '#12264A'}}/>
                 </IconButton> 
+                
             </div>
+            <div id="divSystem"></div>
             </Grid>
           </Grid>
           </div>
-          <div id = "divTeste"></div>
         </div>
-        
+        </>
     );
 }
 }
