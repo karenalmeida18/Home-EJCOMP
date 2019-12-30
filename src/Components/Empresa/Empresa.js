1cdc9e0f5953792764f1ba5296c305b6e0fbc720
Maria Eduarda <duda_panorama@hotmail.com>
2019-12-30 18:24:20 -0300

    coloca a img emp com animation

--- src/Components/Empresa/Empresa.js
+++ src/Components/Empresa/Empresa.js
@@ -3,7 +3,7 @@ import { Button } from 'semantic-ui-react'
 import './Empresa.css'
 import Navbar from '../Navbar/Navbar';
 import Contato from '../Contato/Contato'
-import Logo from '../Images/EJBRANCO.png'
+
 
 export default class Empresa extends React.Component {
     constructor(props) {
@@ -22,7 +22,7 @@ export default class Empresa extends React.Component {
                 <div className="bg-image"></div>
                 <div className="content">
                     <Navbar color='white' logo='logoAzul' className='itemMenu' />
-                    <img src={Logo} id="logo_ej"/>
+                    <div id="logo_ej"> </div> 
                     <h2>A EMPRESA</h2>
                     <div className="text">
                         <p>Fundada em 2006 e reestruturada 10 anos depois, a EJCOMP é uma Empresa Júnior de computação inteiramente composta e gerida por alunos do curso Ciência da Computação, da FCT - Unesp de Presidente Prudente. Em 2016 voltamos à ativa, e em 2018 conquistamos a nossa federação à FEJESP. Assim, passamos a atuar cada vez mais fortes e competentes dentro do estado de São Paulo, e Brasil adentro.</p>
