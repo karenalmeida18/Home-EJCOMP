5b4b150921460ebbc291a1d8131fef3d55ed4e6e
Maria Eduarda <duda_panorama@hotmail.com>
2020-01-03 11:34:44 -0300

    Mudando rotas

---
+++ src/Components/Principal/Principal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Home from '../Home/TelaHome'
+import Mej from '../Mej/Mej'
+import Empresa from '../Empresa/Empresa'
+import Footer from '../Footer/Footer'
+
+export default class Principal extends React.Component{
+    render(){
+        return(
+           <div className='Principal'>
+               <Home/>
+               <Mej/>
+               <Empresa/>
+               <Footer/>
+               
+           </div>
+
+        );
+    }
+}
--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Home from './Components/Home/TelaHome'
-import Mej from './Components/Mej/Mej'
+import Principal from './Components/Principal/Principal'
 import Equipe from './Components/Equipe/Equipe' 
 import Serviços from './Components/Cards/index'
-import Empresa from './Components/Empresa/Empresa'
 import Blog from './Components/Blog/Blog'
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/mej" component={Mej} />
-            <Route exact path="/empresa" component={Empresa} />
+            <Route exact path="/" component={} />
             <Route exact path="/equipe" component={Equipe} />
             <Route exact path="/serviços" component={Serviços} />
             <Route exact path="/blog" component={Blog} />
