c7081b9e5dbb7e83ca4e416955867469ac45302a
Luskas <lucasquintas@hotmail.com>
2020-01-06 22:14:07 -0300

    Navbar dinâmica

--- src/Components/Principal/Principal.js
+++ src/Components/Principal/Principal.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from 'react'
 import Home from '../Home/TelaHome'
 import Empresa from '../Empresa/Empresa'
 import Footer from '../Footer/Footer'
-import TelaMej from '../Mej/Mej';
+import TelaMej from '../Mej/Mej'
+import Navbar from '../Navbar/Navbar'
 
 export default class Principal extends React.Component {
     constructor(props) {
@@ -14,6 +15,7 @@ export default class Principal extends React.Component {
         return (
             <div className='Principal'>
                 <Home id='section1' />
+                <Navbar backgroundColor='white' logo='logoNav' className='itemMenuMej' />
                 <Empresa id='section2' />
                 <TelaMej id='section3' />
                 <Footer />
