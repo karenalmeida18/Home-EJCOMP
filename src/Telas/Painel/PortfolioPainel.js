99ddf85df6673411ea9da2b0f010e2fc80433616
Tilteee <42361545+Tilteee@users.noreply.github.com>
2020-03-16 07:02:48 -0300

    Update Portfolio painel adm

--- src/Telas/Painel/PortfolioPainel.js
+++ src/Telas/Painel/PortfolioPainel.js
@@ -1,20 +1,47 @@
-import React, {Component} from 'react';
+import React ,{Component} from 'react';
 import Usuario from './Usuario';
 import './PortfolioPainel.css';
 
 export default class PortfolioPainel extends Component {
-    render(){
+    render () {
         return (
-            <>
+
             <div>
                 <Usuario/>
-                <div className='PortfolioPainel'>
-                    <div className='PortfolioPost1'>
-                    
+                <div className='portPage'>
+                    <div className='headerPort'><p>Portfolio</p></div>
+                    <div className='containerPort'>
+                        <div>
+                            <div className='sectionPortImg'>
+                                <p>Imagem</p>  
+                                <div className='fotoPort1'></div>
+                                <button className='containerButtons'>Adicionar Imagem</button>
+                                <button className='containerButtons'>Excluir Imagem</button>
+                            </div>
+                            <div className='sectionPortImg'>
+                                <p>Imagem</p>  
+                                <div className='fotoPort2'></div>
+                                <button className='containerButtons'>Adicionar Imagem</button>
+                                <button className='containerButtons'>Excluir Imagem</button>
+                            </div>
+                        </div>
+                        <div className='textsPort'>
+                            <div className='sectionPortTextNome'>
+                                <p>Nome</p>
+                                <input type='excluir' placeholder='Excluir' />
+                                <input type='adicionar' placeholder='Adicionar' />
+                            </div>
+                            <div className='sectionPortTextDesc'>
+                                <p>Descrição</p>
+                                <input type='excluir' placeholder='Excluir' />
+                                <input type='adicionar1' placeholder='Adicionar' />
+                                <button className='concluir'>concluir</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            </>
         )
     }
 }
+
