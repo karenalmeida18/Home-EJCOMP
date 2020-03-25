e653ba3aaf729ef04be5b6d1cbed0649f5cca3f7
Karen Almeida <karenkaka2010@hotmail.com>
2020-03-24 21:17:56 -0300

    validação com EmailJS

--- src/Components/Contato/Contato.js
+++ src/Components/Contato/Contato.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Contato.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import emailjs from 'emailjs-com'
 
 export default class Modal extends React.Component {
     state = {
@@ -12,28 +13,34 @@ export default class Modal extends React.Component {
     }
     handleSubmit = (event) => {
         event.preventDefault();
-        const{email,nome,mensagem,tel} = this.state;
-        if(!email || !nome ||  !mensagem || !tel ){
-         alert("Preencha todos os campos!")
-        }else {
-            this.setState({nome: '', email: '', mensagem: '', tel: '', isVisible: false})
+        const { email, nome, mensagem, tel } = this.state;
+        if (!email || !nome || !mensagem) {
+            alert("Preencha todos os campos!")
+        } else {
+            let templateParams = {
+                user_name: nome,
+                user_email: email,
+                message: mensagem,
+                user_tel: tel
+            }
+            emailjs.send('default_service', 'teste', templateParams, 'user_CJMfwqGMQppqJUM2WvUMs');
+            this.setState({ nome: '', email: '', mensagem: '', tel: '', isVisible: false })
             alert('Mensagem enviada com sucesso!')
-        
         }
     }
-    handleChangeEmail = (e) =>{
-        this.setState ({email: e.target.value,})
+    handleChangeEmail = (e) => {
+        this.setState({ email: e.target.value, })
     }
-    handleChangeNome = (e) =>{
-        this.setState ({nome: e.target.value,})
+    handleChangeNome = (e) => {
+        this.setState({ nome: e.target.value, })
     }
-    handleChangeTel = (e) =>{
-        this.setState ({tel: e.target.value,})
+    handleChangeTel = (e) => {
+        this.setState({ tel: e.target.value, })
     }
-    handleChangeMensagem = (e) =>{
-        this.setState ({mensagem: e.target.value,})
+    handleChangeMensagem = (e) => {
+        this.setState({ mensagem: e.target.value, })
     }
-    render(){
+    render() {
         return (
             <div className="modal" onSubmit={this.handleSubmit}>
                 <form className="modalForms">
@@ -43,16 +50,16 @@ export default class Modal extends React.Component {
                     </div>
                     <div className='contentForms'>
                         <div className='formsColumn1'>
-                            <input type="text" name="name" placeholder="Nome*" 
+                            <input type="text" name="name" placeholder="Nome*"
                                 value={this.state.nome} onChange={this.handleChangeNome} />
-                            <input type="email" name="email" placeholder="E-mail*" 
+                            <input type="email" name="email" placeholder="E-mail*"
                                 value={this.state.email}
                                 onChange={this.handleChangeEmail} />
                             <input type="tel" name="tel" placeholder="Telefone"
                                 value={this.state.tel}
                                 onChange={this.handleChangeTel} />
                         </div>
-                        <textarea type="text" name="text" placeholder="Mensagem*" 
+                        <textarea type="text" name="text" placeholder="Mensagem*"
                             value={this.state.mensagem}
                             onChange={this.handleChangeMensagem} />
                     </div>
