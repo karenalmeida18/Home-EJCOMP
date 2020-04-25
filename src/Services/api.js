16df43bcb829cf7d4d67a2f2066b5f4bdd1f0f0a
unknown <carlos.silva-filho@unesp.br>
2020-04-25 15:50:37 -0300

    alt para upar o site

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://191.252.113.79:5875"
+  baseURL: process.env.API_URL
 });
 
 api.interceptors.request.use(async config => {
